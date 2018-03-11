014c96edf77282deb552bd31703fe74661908c91
jbrun008 <jbrun008@plattsburgh.edu>
2018-03-11 17:21:35 -0400

    p5

--- server.js
+++ server.js
@@ -8,30 +8,32 @@ const twitterService = new twit(twitConfig);
 
 const port = 8080;
 
-app.use('/otter-api', router, express.static(__dirname + '/dist'));
+//app.use('/otter-api', router, express.static(__dirname + '/dist'));
+
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(port, function() {
 	  console.log('Server Started On Port: ' + port);
 	});
 
-router.get('/tweets', function(req, res){
-	var lat = req.query.lat;
-	var lon = req.query.lon;
-	var query = "geocode:" + lat + "," + lon + ",100mi -from:googuns_lulz -from:_grammar_ -jeff_steinport";
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	twitterService.get('search/tweets',{q: query, count: 12}, function(err, data, response){
-		var tweets = [];
-		if (data){
-			for (var i = 0; i < data.statuses.length; i++){
-				tweets.push({user: data.statuses[i].user.screen_name , tweet: data.statuses[i].text});
-			}
-			if (data.statuses.length == 0) {tweets.push({user:"OTTER_SYS", tweet: 'No New Tweets Located'});}
-			res.json(tweets);
-		} 
-		if (err){ console.log(err) };
-  });
-});
+//router.get('/tweets', function(req, res){
+//	var lat = req.query.lat;
+//	var lon = req.query.lon;
+//	var query = "geocode:" + lat + "," + lon + ",100mi -from:googuns_lulz -from:_grammar_ -jeff_steinport";
+//	res.header("Access-Control-Allow-Origin", "*");
+//	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//	twitterService.get('search/tweets',{q: query, count: 12}, function(err, data, response){
+//		var tweets = [];
+//		if (data){
+//			for (var i = 0; i < data.statuses.length; i++){
+//				tweets.push({user: data.statuses[i].user.screen_name , tweet: data.statuses[i].text});
+//			}
+//			if (data.statuses.length == 0) {tweets.push({user:"OTTER_SYS", tweet: 'No New Tweets Located'});}
+//			res.json(tweets);
+//		} 
+//		if (err){ console.log(err) };
+//  });
+//});
 
 
 
--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -15,9 +15,11 @@ export class RestService {
   getPlace(lat: number, lon: number): Observable<any> {
     return this.http.get<any>(`http://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`);
   }
-  getTweets(lat: number, lon: number): Observable<Tweet[]> {
-    return this.http.get<any>(`http://localhost:8080/otter-api/tweets?lat=${lat}&lon=${lon}`);
-  }
+//  getTweets(lat: number, lon: number): Observable<Tweet[]> {
+//    return this.http.get<any>(`http://localhost:8080/otter-api/tweets?lat=${lat}&lon=${lon}`);
+//    
+//  }
+  
 }
 
 
