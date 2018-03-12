2fc0cad5fce4b77965a82ea05e00246745ddcaa0
JTBrunderley <37249398+JTBrunderley@users.noreply.github.com>
2018-03-12 15:35:11 -0400

    Update rest.service.ts

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -13,7 +13,7 @@ export class RestService {
     return this.http.get<IssObject>(`http://api.open-notify.org/iss-now.json`);
   }
   getPlace(lat: number, lon: number): Observable<any> {
-    return this.http.get<any>(`http://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`);
+    return this.http.get<any>(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`);
   }
 //  getTweets(lat: number, lon: number): Observable<Tweet[]> {
 //    return this.http.get<any>(`http://localhost:8080/otter-api/tweets?lat=${lat}&lon=${lon}`);
