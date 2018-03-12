a5aad205604ac2949ee2f8926a72c894d58c4f65
JTBrunderley <37249398+JTBrunderley@users.noreply.github.com>
2018-03-12 15:56:45 -0400

    Update iss-object.model.ts

--- src/app/models/iss-object.model.ts
+++ src/app/models/iss-object.model.ts
@@ -1,5 +1,3 @@
-import {IssPosition} from './iss-position.model';
-
 export class IssObject {
   constructor(public latitude: number,public longitude: number, public altitude: number, public velocity: number, public timestamp: number) {}
 }
