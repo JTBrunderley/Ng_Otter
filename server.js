58e62f4e255e53788df9631ee00913aba6589547
JTBrunderley <37249398+JTBrunderley@users.noreply.github.com>
2018-03-12 16:04:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const twitterService = new twit(twitConfig);
 
 const port = process.env.PORT || 8080;
 
-//app.use('/otter-api', router, express.static(__dirname + '/dist'));
+app.use('/otter-api', router, express.static(__dirname + '/dist'));
 
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
@@ -22,24 +22,24 @@ app.set('port', port);
 const server = http.createServer(app);
 server.listen(port, () => console.log('Server is now Running'));
 
-//router.get('/tweets', function(req, res){
-//	var lat = req.query.lat;
-//	var lon = req.query.lon;
-//	var query = "geocode:" + lat + "," + lon + ",100mi -from:googuns_lulz -from:_grammar_ -jeff_steinport";
-//	res.header("Access-Control-Allow-Origin", "*");
-//	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//	twitterService.get('search/tweets',{q: query, count: 12}, function(err, data, response){
-//		var tweets = [];
-//		if (data){
-//			for (var i = 0; i < data.statuses.length; i++){
-//				tweets.push({user: data.statuses[i].user.screen_name , tweet: data.statuses[i].text});
-//			}
-//			if (data.statuses.length == 0) {tweets.push({user:"OTTER_SYS", tweet: 'No New Tweets Located'});}
-//			res.json(tweets);
-//		} 
-//		if (err){ console.log(err) };
-//  });
-//});
+router.get('/tweets', function(req, res){
+	var lat = req.query.lat;
+	var lon = req.query.lon;
+	var query = "geocode:" + lat + "," + lon + ",100mi -from:googuns_lulz -from:_grammar_ -jeff_steinport";
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	twitterService.get('search/tweets',{q: query, count: 12}, function(err, data, response){
+		var tweets = [];
+		if (data){
+			for (var i = 0; i < data.statuses.length; i++){
+				tweets.push({user: data.statuses[i].user.screen_name , tweet: data.statuses[i].text});
+			}
+			if (data.statuses.length == 0) {tweets.push({user:"OTTER_SYS", tweet: 'No New Tweets Located'});}
+			res.json(tweets);
+		} 
+		if (err){ console.log(err) };
+ });
+});
 
 
 
