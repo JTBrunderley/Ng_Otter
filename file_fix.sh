024a9d3f14577b6e184cf3f4f874471ec02672f0
JTBrunderley <37249398+JTBrunderley@users.noreply.github.com>
2018-03-12 12:36:58 -0400

    Update file_fix.sh

--- file_fix.sh
+++ file_fix.sh
@@ -1,4 +1,3 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
-chmod a+x $SCRIPTPATH/node_modules/p5/lib/p5.d.ts
-sed -i '10315s/.*/    amp(volume: number|object, rampTime?: number, timeFromNow?: number): AudioParam/' $SCRIPTPATH/node_modules/p5/lib/p5.d.ts
+sed -e 's/    amp(volume: number|object, rampTime?: number, timeFromNow?: number): void/    amp(volume: number|object, rampTime?: number, timeFromNow?: number): AudioParam/' $SCRIPTPATH/node_modules/p5/lib/p5.d.ts
