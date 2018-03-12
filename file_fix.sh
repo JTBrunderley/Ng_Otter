eea087b70ee3f816d6f661eb36f8216fbc3b723d
JTBrunderley <37249398+JTBrunderley@users.noreply.github.com>
2018-03-12 12:17:27 -0400

    Update file_fix.sh

--- file_fix.sh
+++ file_fix.sh
@@ -1,3 +1,4 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
+echo $SCRIPTPATH
 sed -e '10315s/.*/    amp(volume: number|object, rampTime?: number, timeFromNow?: number): AudioParam/' -i $SCRIPTPATH/node_modules/p5/lib/p5.d.ts
