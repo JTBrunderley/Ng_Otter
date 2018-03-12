4b4d81f494fdac10044619c522e29139dcd1b70b
JTBrunderley <37249398+JTBrunderley@users.noreply.github.com>
2018-03-12 13:40:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist'));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
 app.set('port', port);
 
 const server = http.createServer(app);
