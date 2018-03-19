0916f89e4fdca4c4a348eb59ada0483e0ff34fad
JTBrunderley <37249398+JTBrunderley@users.noreply.github.com>
2018-03-19 11:43:07 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -62,8 +62,8 @@ export class AppComponent implements OnInit, OnDestroy {
 
   updatePos() {
   this.restService.getPosition().subscribe( (data: PositionObj) => {
-    this.p5Instance1.lat = p5.radians(data.latitude);
-    this.p5Instance1.lon = p5.radians(data.longitude);
+    this.p5Instance1.lat = this.p5Instance2.radians(data.latitude);
+    this.p5Instance1.lon = this.p5Instance2.radians(data.longitude);
   });
   }
 
